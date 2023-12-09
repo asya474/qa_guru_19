01708481e975b3ed8ba07651ee9f5f31b852740a
asya474 <asya474@gmail.com>
2023-12-09 15:56:33 +0700

    add tests for types http requests

--- test.py
+++ test.py
@@ -8,71 +8,39 @@ from utils import load_schema
 
 def test_get_single_user_successfully():
     url = "https://reqres.in/api/users/2"
-    schema = load_schema("get_single_user.json")
+    schema = load_schema("json_schemes/get_single_user.json")
 
     result: Response = requests.get(url)
 
     assert result.status_code == 200
     jsonschema.validate(result.json(), schema)
 
-
-@pytest.mark.parametrize('id_', [1, 2, 3])
-def test_get_single_user_id(id_):
-    url = f"https://reqres.in/api/users/{id_}"
-
-    result = requests.get(url)
-    assert result.json()['data']['id'] == id_
-
-
-def test_list_of_users_pagination():
-    page = 2
+def test_post_create_201_succesfully():
     url = "https://reqres.in/api/users"
+    schema = load_schema("json_schemes/post_create.json")
 
-    result = requests.get(url, params={"page": page})
+    result: Response = requests.post(url)
 
-    assert result.json()["page"] == page
+    assert result.status_code == 201
+    jsonschema.validate(result.json(), schema)
+
+def test_put_update_200_succesfully():
+    url = "https://reqres.in/api/users/2"
+    schema = load_schema("json_schemes/put_update.json")
+
+    result: Response = requests.put(url)
+
+    assert result.status_code == 200
+    jsonschema.validate(result.json(), schema)
+
+def test_delete_204_succesfully():
+    url = "https://reqres.in/api/users/2"
+
+    result: Response = requests.delete(url)
+
+    assert result.status_code == 204
 
 
-def test_list_of_users_per_page():
-    page = 2
-    per_page = 6
-    url = "https://reqres.in/api/users"
-
-    result = requests.get(
-        url=url,
-        params={"page": page, "per_page": per_page}
-    )
-
-    assert result.json()["per_page"] == per_page
-    assert len(result.json()['data']) == per_page
-
-
-def test_get():
-    pass
-
-def test_post():
-    pass
-
-def test_put():
-    pass
-
-def test_delete():
-    pass
-
-def test_positive():
-    pass
-
-def test_negative():
-    pass
-
-def test_200():
-    pass
-
-def test_201():
-    pass
-
-def test_204():
-    pass
 
 def test_404():
     pass
